9f996cd26295938190fa97f1660f3545abe1fe52
Bogdan <bogdan.gros@icloud.com>
2022-09-04 11:31:58 +0200

    Initial Commit

---
+++ Think/ThinkApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThinkApp.swift
+//  Think
+//
+//  Created by 1 on 04/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct ThinkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
